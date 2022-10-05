4c822a39cca05720c431e9309635d7f2c0d0355b
ginovaldocumbe <92864360+ginovaldocumbe@users.noreply.github.com>
2022-10-05 20:54:18 +0200

    tela de registo

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
-import './App.css';
+
 import Login from './pages/login/login'
+import Registo from './pages/registo/registo';
 function App() {
   return (
-    <div className="App">
-      <Login />
+    <div className="h-screen">
+      <Registo />
     </div>
   );
 }
---
+++ src/pages/registo/registo.jsx
@@ -0,0 +1,54 @@
+import React from 'react';
+import Topo from './../../components/top_login/top_login'
+import Baixo from './../../components/baixo/baixo'
+import Butao from '../../components/butao/butao';
+const Registo = () => (
+  <div className="h-full pt-24 ">
+    <Topo />
+    <div className='h-full px-2 overflow-y-scroll'>
+      <h1 className='text-cyan-800 font-bold text-lg mb-8'>Registe-se no SICAP</h1>
+      <form>
+        <div className='flex flex-col mb-4'>
+          <label className='text-gray-400'>Apelido</label>
+          <input type='text' className=' border-b border-gray-400 outline-none focus:outline-offset-2 focus:border-blue-500  text-sm' />
+        </div>
+        <div className='flex flex-col mb-4'>
+          <label className='text-gray-400'>Nomes</label>
+          <input type='text' className=' border-b border-gray-400 outline-none focus:outline-offset-2 focus:border-blue-500  text-sm' />
+        </div>
+        <div className='flex flex-col mb-4'>
+          <label className='text-gray-400'>E-mail</label>
+          <input type='e-mail' className=' border-b border-gray-400 outline-none focus:outline-offset-2 focus:border-blue-500  text-sm' />
+        </div>
+        <div className='flex flex-col mb-4'>
+          <label className='text-gray-400'>Contacto</label>
+          <input type='number' className=' border-b border-gray-400 outline-none focus:outline-offset-2 focus:border-blue-500  text-sm' />
+        </div>
+        <div className='flex flex-col mb-4'>
+          <label className='text-gray-400'>Morada</label>
+          <input type='text' className=' border-b border-gray-400 outline-none focus:outline-offset-2 focus:border-blue-500  text-sm' />
+        </div>
+        <div className='flex flex-col mb-4'>
+          <label className='text-gray-400'>Senha</label>
+          <input type='password' className=' border-b border-gray-400 outline-none focus:outline-offset-2 focus:border-blue-500  text-sm' />
+        </div>
+        <div className='flex flex-col mb-4'>
+          <label className='text-gray-400'>Confirme a senha</label>
+          <input type='password' className=' border-b border-gray-400 outline-none focus:outline-offset-2 focus:border-blue-500  text-sm' />
+        </div>
+        <div className='flex flex-row items-center justify-center'>
+          <Butao funcao="Registar" />
+          <button className='bg-gray-500 ml-8  '>Voltar</button>
+        </div>
+      </form>
+
+    </div>
+    <Baixo />
+  </div>
+);
+
+Registo.propTypes = {};
+
+Registo.defaultProps = {};
+
+export default Registo;
