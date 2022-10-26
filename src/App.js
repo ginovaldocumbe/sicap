1b5f218b1a36591f42d7dd772e279e4d223815c7
ginovaldocumbe <92864360+ginovaldocumbe@users.noreply.github.com>
2022-10-26 13:22:36 +0200

    detalhes da publicacao

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@
 import Login from './pages/login/login'
 import Registo from './pages/registo/registo';
 import Detalhes from './pages/detalhes/detalhes';
+import DetalhesPub from './pages/detalhesPub/detalhes'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
         <Route path='/' element={<Login />} />
         <Route path='/registo' element={<Registo />} />
         <Route path='/meus_detalhes' element={<Detalhes />} />
+        <Route path='/detalhes_publicacao' element={<DetalhesPub />} />
       </Routes>
     </Router>
   );
--- src/pages/detalhesPub/detalhes.jsx
+++ src/pages/detalhesPub/detalhes.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import Topo from './../../components/top_login/top_login'
-import Baixo from './../../components/baixo/baixo'
+import Topo from '../../components/top_login/top_login'
+import Baixo from '../../components/baixo/baixo'
 import { BsThreeDotsVertical } from "react-icons/bs";
 import Iphone from './../../imagens/iphone.jpg';
 
@@ -12,7 +12,7 @@ const Detalhes = () => (
         <img className='h-full rounded-lg' src={Iphone}/>
       </div>
       <div className='-mt-8 border py-5 bg-white rounded-t-[40px] z-20'>
-        <h1 className='flex justify-center font-bold text-xl'>Detalhes</h1>
+        <h1 className='flex justify-center font-bold text-xl'>Detalhes do Item</h1>
         <div className='flex justify-between px-3'>
           <span>Tipo: </span>
           <label>B.I</label>
@@ -38,18 +38,8 @@ const Detalhes = () => (
            </label>
         </div>
         <div className='h-px my-2 bg-gradient-to-r from-sky-600 via-black to-sky-600'></div>
-        <div>
-          <p className='text-sm font-bold'>Alterar estado</p>
-          <div className='flex flex-row items-center justify-between pr-2'>
-            <select className='text-sm'>
-              <option>Perdido</option>
-              <option>Achado</option>
-            </select>
-            <BsThreeDotsVertical />
-          </div>
-        </div>
         <div className='flex justify-center mt-8'>
-          <button className='mr-6 bg-sky-900 px-12 py-2 rounded-md text-white'>Salvar</button>
+          <button className='mr-6 bg-sky-900 w-42 px-3 py-2 rounded-md text-white'>Pedir devolucao</button>
           <button className='px-12 py-2 rounded-md bg-blue-200 text-black'>Voltar</button>
         </div>
       </div>
